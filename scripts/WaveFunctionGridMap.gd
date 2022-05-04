0281a91c1d63cd66891ed51d324955cbeceeb27c
Adam Spruijt <adamspruijt@gmail.com>
2022-05-03 21:46:37 -0400

    work

--- scripts/WaveFunctionGridMap.gd
+++ scripts/WaveFunctionGridMap.gd
@@ -6,35 +6,13 @@ const FILE_PROTOTYPES = "res://resources/prototypes.json"
 const FILE_CELLS = "res://resources/cells.json"
 const FILE_SOCKETS = "res://resources/sockets.json"
 const FILE_REGISTRY = "res://resources/sockets_registry.json"
+const FILE_TEST = "res://resources/test.json"
 
 export var clear_canvas : bool setget set_clear_canvas
 export var export_definitions : bool = false setget set_export_definitions
 
 var template : WaveFunctionTemplateResource
 
-
-const orientations = {
-	Vector3.FORWARD: 0,
-	Vector3.RIGHT: 22,
-	Vector3.BACK: 10,
-	Vector3.LEFT: 16
-}
-
-const oriented_directions = {
-	0 : {
-		Vector3.FORWARD: Vector3.FORWARD,
-		Vector3.RIGHT: Vector3.RIGHT,
-		Vector3.BACK: Vector3.BACK,
-		Vector3.LEFT: Vector3.LEFT
-	},
-	22 : {
-		Vector3.FORWARD: Vector3.FORWARD,
-		Vector3.RIGHT: Vector3.RIGHT,
-		Vector3.BACK: Vector3.BACK,
-		Vector3.LEFT: Vector3.LEFT
-	},
-
-}
 
 const sibling_directions = {
 	Vector3.RIGHT : 'right',
@@ -50,61 +28,93 @@ var structure := {}
 
 func get_oriented_directions(cell_orientation: int) -> Dictionary:
 	var directions : Dictionary
+
 	match cell_orientation:
 		0: # Forward (default)
-			directions = {
+			return {
 				Vector3.FORWARD: Vector3.FORWARD,
 				Vector3.RIGHT: Vector3.RIGHT,
 				Vector3.BACK: Vector3.BACK,
 				Vector3.LEFT: Vector3.LEFT
 			}
 		22: # Right (90)
-			directions = {
-				Vector3.FORWARD: Vector3.LEFT,
-				Vector3.RIGHT: Vector3.FORWARD,
-				Vector3.BACK: Vector3.RIGHT,
-				Vector3.LEFT: Vector3.BACK
+			return  {
+				Vector3.FORWARD: Vector3.RIGHT,
+				Vector3.RIGHT: Vector3.BACK,
+				Vector3.BACK: Vector3.LEFT,
+				Vector3.LEFT: Vector3.FORWARD
 			}
 		10: # Back (180)
-			directions = {
+			return  {
 				Vector3.FORWARD: Vector3.BACK,
 				Vector3.RIGHT: Vector3.LEFT,
 				Vector3.BACK: Vector3.FORWARD,
 				Vector3.LEFT: Vector3.RIGHT
 			}
 		16: # Left (270)
-			directions = {
+			return  {
 				Vector3.FORWARD: Vector3.RIGHT,
 				Vector3.RIGHT: Vector3.BACK,
 				Vector3.BACK: Vector3.LEFT,
 				Vector3.LEFT: Vector3.FORWARD
 			}
-
-	return directions
+		_:
+			return {}
 
 
 func update_prototypes() -> void:
 	template = WaveFunctionTemplateResource.new()
 	var used_cells := get_used_cells()
 
-	for cell_coordinates in used_cells:
-		var cell_index := get_cell_item(cell_coordinates.x,cell_coordinates.y,cell_coordinates.z)
-		var cell_orientation := get_cell_item_orientation(cell_coordinates.x,cell_coordinates.y,cell_coordinates.z)
+	for coords in used_cells:
+		var cell_index := get_cell_item(coords.x,coords.y,coords.z)
+		var cell_orientation := get_cell_item_orientation(coords.x,coords.y,coords.z)
 		print("cell: %s_%s" % [cell_index,cell_orientation] )
 
 		if not structure.has(cell_index):
 			structure[cell_index] = {}
 
-		var directions : Dictionary = get_oriented_directions(cell_orientation)
-		print(directions)
+		if not structure[cell_index].has(cell_orientation):
+			structure[cell_index][cell_orientation] = {}
+
+
+		var oriented_directions : Dictionary = get_oriented_directions(cell_orientation)
+		print(oriented_directions)
 #		if not structure[cell_index].has()
 
 		for direction in sibling_directions:
+			var sibling_coords = coords + direction
+			var sibling_cell_index := get_cell_item(sibling_coords.x,sibling_coords.y,sibling_coords.z)
+			var sibling_cell_orientation := get_cell_item_orientation(sibling_coords.x,sibling_coords.y,sibling_coords.z)
+			var direction_name = sibling_directions[direction]
+			var oriented_direction = Vector3.ZERO
+			var oriented_direction_name = ''
 
-			var sibling_cell_index := get_cell_item(cell_coordinates.x,cell_coordinates.y,cell_coordinates.z)
-			var sibling_cell_orientation := get_cell_item_orientation(cell_coordinates.x,cell_coordinates.y,cell_coordinates.z)
+			if oriented_directions.has(direction):
+				oriented_direction = oriented_directions[direction]
+				oriented_direction_name = sibling_directions[oriented_direction]
 
-#		add_cell_prototype(cell_coordinates,cell_index, cell_orientation)
+			if not oriented_direction_name:
+				continue
+
+
+			if not structure[cell_index][cell_orientation].has(direction_name):
+				structure[cell_index][cell_orientation][direction_name] = {}
+
+			if not structure[cell_index][cell_orientation][direction_name].has(sibling_cell_index):
+				structure[cell_index][cell_orientation][direction_name][sibling_cell_index] = []
+
+			if not structure[cell_index][cell_orientation][direction_name][sibling_cell_index].has(sibling_cell_orientation):
+				structure[cell_index][cell_orientation][direction_name][sibling_cell_index].append(sibling_cell_orientation)
+
+			print('	siblings( %s ): %s' % [direction_name, oriented_direction_name])
+
+
+
+	var file = File.new()
+	file.open(FILE_TEST, File.WRITE)
+	file.store_line(to_json(structure))
+	file.close()
 
 #	print_debug("Generated prototype: %s cells in use." % used_cells.size())
 
